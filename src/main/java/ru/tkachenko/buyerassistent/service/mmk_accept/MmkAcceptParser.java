e216142fb1d41523058ae6ed55f028fd70b99e98
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2022-04-12 13:28:08 +0500

    added MmkAcceptParser - in progress

--- src/main/java/ru/tkachenko/buyerassistent/service/mmk_accept/MmkAcceptParser.java
+++ src/main/java/ru/tkachenko/buyerassistent/service/mmk_accept/MmkAcceptParser.java
@@ -1,10 +1,11 @@
 package ru.tkachenko.buyerassistent.service.mmk_accept;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
-import ru.tkachenko.buyerassistent.entity.MmkAcceptRowEntity;
 import ru.tkachenko.buyerassistent.utils.ExcelUtils;
 
 import java.io.FileInputStream;
@@ -13,7 +14,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 @Service
-public class MmkAcceptRowParser {
+public class MmkAcceptParser {
     private final String SPEC_COL_NAME = "Номер заказа";
     private final String POSITION_COL_NAME = "Номер строки";
     private final String NOMENCLATURE_COL_NAME = "Наименование позиции";
@@ -30,23 +31,36 @@ public class MmkAcceptRowParser {
             THICKNESS_COL_NAME, WIDTH_COL_NAME, LENGTH_COL_NAME, ALTER_PROFILE_COL_NAME, ACCEPTED_COL_NAME,
             ACCEPTED_MONTH_COL_NAME, ADDITIONAL_REQUIREMENTS_COL_NAME};
 
-    public MmkAcceptRowEntity parseRowEntity (Path filePath) {
+    public void parseFile (Path filePath) {
         try {
             FileInputStream fileInputStream = new FileInputStream(filePath.toString());
             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
             XSSFSheet sheet = workbook.getSheetAt(0);
             int headerRowIndex = ExcelUtils.findFirstNotBlankRow(sheet);
-            XSSFRow header = sheet.getRow(headerRowIndex);
-            int[] colIndexes = ExcelUtils.findColIndexesByValues(columnsNames, header);
+            int[] colIndexes = getEntityColumns(sheet, headerRowIndex);
+
+            int firstRowIndex = headerRowIndex + 1;
+            int lastRowIndex = sheet.getLastRowNum();
+
+            for (int i = firstRowIndex; i <= lastRowIndex; i++) {
+                Row currentRow = sheet.getRow(i);
+                parseMmkAcceptEntityFromRow(colIndexes, currentRow);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
 
-        
+    }
 
-        //TODO dont forget to return MmkAcceptRowEntity
-        return null;
+    private int[] getEntityColumns(Sheet sheet, int headerRowIndex) {
+        Row header = sheet.getRow(headerRowIndex);
+        int[] colIndexes = ExcelUtils.findColIndexesByValues(columnsNames, header);
+        return colIndexes;
+    }
+
+    private void parseMmkAcceptEntityFromRow(int[] colIndexes, Row row) {
+
     }
 }
